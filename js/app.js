3f3d10e903e554f4eff44f248d5b587f3c5bd0c3
Fubi00 <fabian.alsaker13@gmail.com>
2024-12-07 21:18:46 +0100

    Fikset "timerInput" til "timeInput" for at html og css matcher. Flyttet litt rundt på koden og fikset noen komentarer

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-let time = 25 * 60; // 25 minutter
+let time = 25 * 60; // Global variabel for total tid
 let timerInterval;
+const progressBar = document.getElementById("progressBar");
+progressBar.style.backgroundColor = time / totalTime > 0.5 ? "green" : "red";
+
 
 function playSound() {
     const audio = new Audio('alarm.mp3');
     audio.play();
   }
 
-function startTimer() {
+  function startTimer() {
     const userInput = document.getElementById("timeInput").value;
-    time = userInput ? userInput * 60 : 25 * 60; // Standard til 25 minutter om ingen input
+    totalTime = userInput ? userInput * 60 : 25 * 60; // Total tid basert på input
+    time = totalTime; // Sett `time` til total tid
     timerInterval = setInterval(() => {
       const minutes = Math.floor(time / 60);
       const seconds = time % 60;
@@ -24,7 +28,7 @@ function startTimer() {
         alert("Tid er ute!");
       }
     }, 1000);
-  }
+}
 
 document.getElementById("startButton").addEventListener("click", startTimer);
 document.getElementById("pauseButton").addEventListener("click", () => {
